95e8936aef33c218e7437918791f3a5e5405dae6
sanilshrmiu <129837713+sanilshrmiu@users.noreply.github.com>
2023-07-07 00:37:14 -0500

    Initial Project Setup with Aws and DynamoDB dependencies

---
+++ src/main/java/dynamo/db/dynamodblocalstack/DynamoDbLocalStackApplication.java
@@ -0,0 +1,13 @@
+package dynamo.db.dynamodblocalstack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DynamoDbLocalStackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DynamoDbLocalStackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dynamo/db/dynamodblocalstack/DynamoDbLocalStackApplicationTests.java
@@ -0,0 +1,13 @@
+package dynamo.db.dynamodblocalstack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DynamoDbLocalStackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
